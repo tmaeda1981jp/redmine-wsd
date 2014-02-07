ca4c66cc22dcd4d632e8c0491cf5570386724bc5
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-02-08 02:48:54 +0900

    Initial commit

---
+++ init.rb
@@ -0,0 +1,26 @@
+Redmine::Plugin.register :redmine_wsd do
+  name 'Redmine Sequencedialog Macros plugin'
+  author 'tmaeda1981jp'
+  description 'Redmine wiki page macros for www.websequencediagrams.com'
+  version '0.0.1'
+  url 'https://github.com/tmaeda1981jp/redmine-wsd'
+  author_url 'https://github.com/tmaeda1981jp'
+end
+
+Redmine::WikiFormatting::Macros.register do
+  desc 'Redmine wiki page macros for www.websequencediagrams.com'
+  macro :seq do |obj, args, text|
+    raise 'Invalid arguments' if args.length > 2
+    response = Net::HTTP.post_form(
+      URI.parse('http://www.websequencediagrams.com/index.php'),
+      'style' => args[1] || 'patent',
+      'message' => text)
+    result = YAML.load(response.body)
+    raise result['errors'].join(',') if result['errors'].any?
+
+    return image_tag(
+      "http://www.websequencediagrams.com/#{result['img']}",
+      :width => args[0] || '480px')
+
+  end
+end
